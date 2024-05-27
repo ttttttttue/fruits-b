896207504c0cc027ea17291562733fa2eb5c3b03
Anton <Krasikov-94@bk.ru>
2024-05-27 22:22:49 +0300

    add Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import {  Module } from '@nestjs/common';
 import { FruitsModule } from './fruits/fruits.module';
 import { VegetablesModule } from './vegetables/vegetables.module';
-import { CorsMiddleware } from '../cors.middleware';
+
 
 @Module({
   imports: [FruitsModule, VegetablesModule],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(CorsMiddleware)
-      .forRoutes({ path: '*', method: RequestMethod.ALL });
-  }
+
+
 }
