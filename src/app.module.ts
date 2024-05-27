aa9a740e8ff6eee0da04d4e9b0241f0ddeead8ac
Anton <Krasikov-94@bk.ru>
2024-05-27 22:41:56 +0300

    edit app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FruitsModule } from './fruits/fruits.module';
 import { VegetablesModule } from './vegetables/vegetables.module';
+import { MiddlewareConsumer } from '@nestjs/common';
+import * as cors from 'cors';
 
 @Module({
   imports: [FruitsModule, VegetablesModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors()).forRoutes('*');
+  }
+}
