ce29e2ab4410540a7241c1efc2c711172cde7d6c
Anton <Krasikov-94@bk.ru>
2024-05-27 22:26:10 +0300

    add Middleware

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000', // Укажите здесь адрес вашего фронтенда
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
 
   await app.listen(3000);
 }
